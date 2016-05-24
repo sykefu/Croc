48050904b80a18b0232c1b90354a5bbe703e2e35
Tristan <Brekekekeke@users.noreply.github.com>
2016-05-24 17:26:44 +0200

    Delete RemotePlayer.java

--- RemotePlayer.java
+++
@@ -1,9 +0,0 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-//client-side interface
-public interface RemotePlayer extends Remote {
-	public void setlastPlayedCard(int i) throws RemoteException;
-	
-	//declare here every method needed by client for display or control
-}
